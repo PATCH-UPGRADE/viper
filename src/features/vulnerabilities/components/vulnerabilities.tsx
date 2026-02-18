bdf2e02c1ba5c9db27ed5e03fd326f8cd54cd88a
Cassidy Diamond <cassidy.diamond@bugcrowd.com>
2026-02-18 14:38:29 -0500

    resizing tailwind metrics

--- src/features/vulnerabilities/components/vulnerabilities.tsx
+++ src/features/vulnerabilities/components/vulnerabilities.tsx
@@ -167,7 +167,7 @@ export const VulnerabilitiesByPriorityMetrics = ({
   data: VulnerabilitiesByPriorityCounts;
 }) => {
   return (
-    <div className="*:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card grid grid-cols-1 gap-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:shadow-xs lg:grid-cols-5">
+    <div className="*:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card grid grid-cols-2 gap-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:shadow-xs md:grid-cols-3 2xl:grid-cols-5">
       {Object.entries(data).map(([key, value]) => {
         const explained =
           PrioritiesExplained[key as keyof typeof PrioritiesExplained];

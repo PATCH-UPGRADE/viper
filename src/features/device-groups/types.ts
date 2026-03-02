61045d9b14e0913d5104db2ba80049f9ec71771c
trummelhadron <150296284+trummelhadron@users.noreply.github.com>
2026-03-02 10:40:21 -0700

    [VW-72] Optionally filter DeviceGroup getMany by updatedBy
    Co-authored-by: taylor <taylor@hadron.lan>

--- src/features/device-groups/types.ts
+++ src/features/device-groups/types.ts
@@ -1,10 +1,42 @@
 import { z } from "zod";
+import {
+  createPaginatedResponseSchema,
+  paginationInputSchema,
+} from "@/lib/pagination";
 
 export const deviceGroupSchema = z.object({
   id: z.string(),
   cpe: z.string(),
 });
+
+export const paginationInputWithUpdatedAtFilterFields =
+  paginationInputSchema.extend({
+    updatedAtStartTime: z.date().optional(),
+    updatedAtEndTime: z.date().optional(),
+  });
+
+export const deviceGroupInputSchema = z
+  .object({
+    id: z.string(),
+    manufacturer: z.string().nullable().optional(),
+    modelName: z.string().nullable().optional(),
+    version: z.string().nullable().optional(),
+  })
+  .refine(
+    (data) =>
+      data.manufacturer !== undefined ||
+      data.modelName !== undefined ||
+      data.version !== undefined,
+    { message: "At least one field must be provided." },
+  );
+
+export const deviceGroupInputHelmIdSchema = z.object({
+  id: z.string(),
+  helmSbomId: z.string(),
+});
+
 export type DeviceGroupIncludeType = z.infer<typeof deviceGroupSchema>;
+
 export const deviceGroupWithUrlsSchema = deviceGroupSchema.extend({
   url: z.string(),
   sbomUrl: z.string().nullable(), // TODO: VW-54
@@ -12,16 +44,23 @@ export const deviceGroupWithUrlsSchema = deviceGroupSchema.extend({
   deviceArtifactsUrl: z.string(),
   assetsUrl: z.string(),
 });
+
 export type DeviceGroupWithUrls = z.infer<typeof deviceGroupWithUrlsSchema>;
+
 export const deviceGroupWithDetailsSchema = deviceGroupWithUrlsSchema.extend({
   manufacturer: z.string().nullable(),
   modelName: z.string().nullable(),
   version: z.string().nullable(),
   helmSbomId: z.string().nullable(),
 });
+
 export type DeviceGroupWithDetails = z.infer<
   typeof deviceGroupWithDetailsSchema
 >;
+
+export const paginatedDeviceGroupResponseSchema = createPaginatedResponseSchema(
+  deviceGroupWithDetailsSchema,
+);
 
 export const deviceGroupSelect = {
   select: {

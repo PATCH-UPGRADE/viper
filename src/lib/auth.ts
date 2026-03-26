ece3b969c1375f0cb207fb2316e84407eb0e0e5a
Cassidy Diamond <cassidy.diamond@bugcrowd.com>
2026-03-26 16:03:03 -0400

    lint and format :/

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -86,7 +86,7 @@ export const auth = betterAuth({
     github: {
       clientId: process.env.GITHUB_OAUTH_CLIENT_ID as string,
       clientSecret: process.env.GITHUB_OAUTH_CLIENT_SECRET as string,
-    }
+    },
   },
   trustedOrigins: [
     "http://localhost:3000",

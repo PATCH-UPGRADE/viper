2a46b4dc8378690aec9d6ac7a77bdce7af140c8e
taylor <taylor@hadron.lan>
2026-03-27 11:33:23 -0600

    better error and loading headers

--- src/app/(dashboard)/(rest)/connectors/[resourceTypeUrl]/items/page.tsx
+++ src/app/(dashboard)/(rest)/connectors/[resourceTypeUrl]/items/page.tsx
@@ -3,21 +3,32 @@ import type React from "react";
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 import {
-  ConnectorsError,
-  ConnectorsLoading,
-} from "@/features/api-key-connectors/components/connectors";
-import { AssetsList } from "@/features/assets/components/assets";
+  AssetsError,
+  AssetsList,
+  AssetsLoading,
+} from "@/features/assets/components/assets";
 import { prefetchAssets } from "@/features/assets/server/prefetch";
-import { DeviceArtifactsDataList } from "@/features/device-artifacts/components/device-artifacts";
+import {
+  DeviceArtifactsDataList,
+  DeviceArtifactsError,
+} from "@/features/device-artifacts/components/device-artifacts";
 import { prefetchDeviceArtifacts } from "@/features/device-artifacts/server/prefetch";
 import { paginationParamsLoader } from "@/features/integrations/server/params-loader";
 import {
   integrationsMapping,
   isValidResourceTypeKey,
 } from "@/features/integrations/types";
-import { RemediationsDataList } from "@/features/remediations/components/remediations";
+import {
+  RemediationsDataList,
+  RemediationsError,
+  RemediationsLoading,
+} from "@/features/remediations/components/remediations";
 import { prefetchRemediations } from "@/features/remediations/server/prefetch";
-import { VulnerabilitiesList } from "@/features/vulnerabilities/components/vulnerabilities";
+import {
+  VulnerabilitiesError,
+  VulnerabilitiesList,
+  VulnerabilitiesLoading,
+} from "@/features/vulnerabilities/components/vulnerabilities";
 import { prefetchVulnerabilities } from "@/features/vulnerabilities/server/prefetch";
 import { ResourceType } from "@/generated/prisma";
 import { requireAuth } from "@/lib/auth-utils";
@@ -26,25 +37,35 @@ import type { PaginationInput } from "@/lib/pagination";
 import { HydrateClient } from "@/trpc/server";
 
 interface ConnectorResourceTypeConfig {
+  errorElement: () => React.ReactNode;
   listElement: React.FC;
+  loadingElement: () => React.ReactNode;
   prefetch: (params: PaginationInput) => void;
 }
 
 const LIST_MAPPING: Record<string, ConnectorResourceTypeConfig> = {
   [ResourceType.Asset]: {
+    errorElement: AssetsError,
     listElement: AssetsList,
+    loadingElement: AssetsLoading,
     prefetch: prefetchAssets,
   },
   [ResourceType.DeviceArtifact]: {
+    errorElement: DeviceArtifactsError,
     listElement: DeviceArtifactsDataList,
+    loadingElement: VulnerabilitiesLoading,
     prefetch: prefetchDeviceArtifacts,
   },
   [ResourceType.Remediation]: {
+    errorElement: RemediationsError,
     listElement: RemediationsDataList,
+    loadingElement: RemediationsLoading,
     prefetch: prefetchRemediations,
   },
   [ResourceType.Vulnerability]: {
+    errorElement: VulnerabilitiesError,
     listElement: VulnerabilitiesList,
+    loadingElement: VulnerabilitiesLoading,
     prefetch: prefetchVulnerabilities,
   },
 };
@@ -62,15 +83,14 @@ const Page = async ({
 
   const resourceType = integrationsMapping[resourceTypeUrl].type;
   const config = LIST_MAPPING[resourceType];
-  const ListElement = config.listElement;
   const paginationParams = await paginationParamsLoader(searchParams);
   await config.prefetch({ ...paginationParams });
 
   return (
     <HydrateClient>
-      <ErrorBoundary fallback={<ConnectorsError />}>
-        <Suspense fallback={<ConnectorsLoading />}>
-          <ListElement />
+      <ErrorBoundary fallback={<config.errorElement />}>
+        <Suspense fallback={<config.loadingElement />}>
+          <config.listElement />
         </Suspense>
       </ErrorBoundary>
     </HydrateClient>

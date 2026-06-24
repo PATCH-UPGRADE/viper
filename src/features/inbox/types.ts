b600697ce31c503d78f671db9cb81a021bf84ba1
Cassidy Diamond <102189185+0xcad@users.noreply.github.com>
2026-06-24 11:20:53 -0400

    [VW-306] User can forward an email address to the VMP for parsing (#142)
    User can forward an email address to the VMP via Resend. This kicks off a service that
    
    * Use an LLM to determine if the notification is relevant
    * Saves the email, and any attachments, in the database. Uses minio locally for S3 instead of AWS
    * Use an LLM to create a normalized Notification in the database
    * Use an LLM to extract device groups from the email
    * Use an LLM to, from those extracted device groups, either relate those to the Notification, update them with new info, or create them
    
    End result: Notification object on VIPER with relevant DeviceGroup's linked

---
+++ src/features/inbox/types.ts
@@ -0,0 +1,13 @@
+import { z } from "zod";
+
+// TODO: Get a discriminated union of what fields should actually live on Advisory|Recall|UpdateAvailable (see `details`)
+export const notificationPayloadSchema = z.object({
+  type: z.enum(["Advisory", "Recall", "UpdateAvailable", "Other"]),
+  title: z.string(),
+  summary: z.string(),
+  tlp: z
+    .enum(["WHITE", "GREEN", "AMBER", "RED", "CLEAR", "AMBER_STRICT"])
+    .nullable(),
+});
+
+export type NotificationPayload = z.infer<typeof notificationPayloadSchema>;

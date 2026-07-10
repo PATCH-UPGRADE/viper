09ea903af904de9b077251fcc208d0a3ae5de5cf
Perry Sy <perrydev17@gmail.com>
2026-07-10 12:25:06 -0400

    update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators: false,
+  serverExternalPackages: ["playwright-core", "@sparticuz/chromium"],
   async redirects() {
     return [
       {

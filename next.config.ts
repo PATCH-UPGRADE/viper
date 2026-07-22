3b0f69c59b0018e19380ca6c05ed4699bc5e30fc
Mark Glose <mark.glose@bugcrowd.com>
2026-07-22 11:02:46 -0500

    [VW-333] Modifying nextJS config in an attempt to fix the streaming issue

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators: false,
+  compress: false,
   serverExternalPackages: ["playwright-core", "@sparticuz/chromium"],
   outputFileTracingIncludes: {
     "/api/inngest/route": [

7026f6ff7902e64d230c40f161dce8c71517895f
Perry Sy <perrydev17@gmail.com>
2026-08-14 19:25:11 -0400

    add auth, sessions handling for platforms teamplay-fleet

---
+++ src/features/integrations/platforms/teamplay-fleet/config.ts
@@ -0,0 +1,38 @@
+import { z } from "zod";
+import { authCredentialSchema } from "@/features/integrations/core/credentials";
+import { AuthType } from "@/generated/prisma";
+import { FLEET_HOST } from "./urls";
+import { basicAuthSchema } from "@/lib/schemas";
+
+export const SIEMENS_HEALTHINEERS = "Siemens Healthineers";
+export const BASE_URL = `https://${FLEET_HOST}`;
+
+export const configSchema = z.object({});
+export type FleetConfig = z.infer<typeof configSchema>;
+
+export const credentialSchema = authCredentialSchema;
+export type FleetCreds = z.infer<typeof credentialSchema>;
+export type FleetLogin = z.infer<typeof basicAuthSchema>;
+
+export interface SessionLoginConfig {
+  welcomeUrl: string;
+  cookieBannerAcceptSelector?: string; // there is a cookie setting overlay on the very first login
+  welcomeLoginButtonSelector: string;
+  userNameSelector: string;
+  continueSelector: string;
+  passwordSelector: string;
+  submitSelector: string;
+  cookieOrigin: string;
+  authUrl: string;
+}
+
+export function loginCredentials(creds: FleetCreds) {
+  if (creds.authType !== AuthType.Basic) {
+    throw new Error(`Teamplay Fleet login error`);
+  }
+  const parsed = basicAuthSchema.safeParse(creds.authentication);
+  if (!parsed.success) {
+    throw new Error(`Teamplay Fleet credentials missing`);
+  }
+  return parsed.data;
+}

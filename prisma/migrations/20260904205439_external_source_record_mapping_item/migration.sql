5c683f8bc1d252895a7bd4393336bb39471560bd
Perry Sy <perrydev17@gmail.com>
2026-09-07 20:48:25 -0400

    add new column in ExternalSourceRecordMapping

---
+++ prisma/migrations/20260904205439_external_source_record_mapping_item/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "external_source_record_mappings" ADD COLUMN     "itemId" TEXT;
+
+-- CreateIndex
+CREATE INDEX "external_source_record_mappings_itemId_idx" ON "external_source_record_mappings"("itemId");
+
+-- AddForeignKey
+ALTER TABLE "external_source_record_mappings" ADD CONSTRAINT "external_source_record_mappings_itemId_fkey" FOREIGN KEY ("itemId") REFERENCES "source_record"("id") ON DELETE CASCADE ON UPDATE CASCADE;

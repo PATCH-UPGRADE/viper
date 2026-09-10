0956b478b3af10c5fc449eaa290a509291f31756
Cassidy Diamond <102189185+0xcad@users.noreply.github.com>
2026-09-10 14:57:01 -0400

    [VW-433] use existing mapping field instead of mappings (#286)

--- prisma/migrations/20260908211426_externam/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "external_source_record_mappings" ADD COLUMN     "itemId" TEXT;
-
--- CreateIndex
-CREATE INDEX "external_source_record_mappings_itemId_idx" ON "external_source_record_mappings"("itemId");
-
--- AddForeignKey
-ALTER TABLE "external_source_record_mappings" ADD CONSTRAINT "external_source_record_mappings_itemId_fkey" FOREIGN KEY ("itemId") REFERENCES "source_record"("id") ON DELETE SET NULL ON UPDATE CASCADE;
